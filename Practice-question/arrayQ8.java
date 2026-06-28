aa37727adc1f0704054c01d37e13719f13d9b5b1
mansipundirr <pundirmansi59@gmail.com>
2026-06-28 19:15:47 +0530

    Hi

---
+++ Practice-question/arrayQ8.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class arrayQ8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+    
+        System.out.println("Enter the array elements :");
+        for(int i = 0; i < 5; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0; i < 5; i++)
+        {
+            for(int j = i + 1; j < arr.length; j++)
+            {
+                if(arr[i] > arr[j])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+               
+                 
+            
+        }
+             System.out.println("Assending order " + arr[i] + " ");
+        
+            }
+            
+
+        //    for( int i = 0; i < 5; i++)
+        //       {
+            // System.out.println("Assending order " + arr[i]+ " ");
+             
+    }
+}
+    
+        
+    
+
--- function/Array/array.java
+++ function/Array/array.java
@@ -11,7 +11,7 @@ public class array {
         }
 
        
-        for(int i=0; i<arr.length; i++){
+        for(int i=0; i <= arr.length; i++){
             System.out.print(arr[i] + " ");
         }
     }
